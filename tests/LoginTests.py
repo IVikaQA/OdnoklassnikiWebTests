87fa636a233b00da07f37901272be96e287d906a
IVika-QA <apple@MacBook-Pro-Apple.local>
2024-12-10 21:49:43 +0300

    Ничего не делала

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,11 +1,16 @@
 from core.BaseTest import browser
-from pages.BasePage import BasePage
+from pages.BasePage import BasePageHelper
 from pages.LoginPage import LoginPageHelper
 import allure
 
+from tests.RecoveryTests import LOGIN_TEXT
+
 BASE_URL = "https://ok.ru/"
+LOGIN_TEXT
 EMPTY_LOGIN_ERROR = "Введите логин"
 EMPTY_PASSWORD_ERROR = "Введите пароль"
+WRONG_PASSWORD = '1'
+
 
 #Тест 1
 @allure.suite('Проверка формы авторизации')
@@ -16,7 +21,7 @@ def test_empty_login_and_password(browser):
     #1.Инициализация страницы: Вызывается конструктор класса-родителя BasePage, который,
     # содержит общие методы для работы со страницами. Сейчас вызывается метод get_url(BASE_URL),
     # который открывает указанный URL (в данном случае, базовый URL для страницы авторизации).
-    BasePage(browser).get_url(BASE_URL)
+    BasePageHelper(browser).get_url(BASE_URL)
     #2. Создается экземпляр класса LoginPageHelper, который содержит методы для взаимодействия
     # с элементами на странице логина.
     LoginPage = LoginPageHelper(browser)
@@ -32,7 +37,7 @@ def test_empty_login_and_password(browser):
 @allure.feature('Тест Login Page')
 @allure.story('Негативный:Аутентификация:Не заполняем пароль')
 def test_empty_password(browser):
-    BasePage(browser).get_url(BASE_URL)
+    BasePageHelper(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.push_text('MyLogin')
     LoginPage.click_login()
