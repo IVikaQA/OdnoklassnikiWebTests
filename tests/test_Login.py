ee56875839a2182e8a14a7d285d7afcd2479bb40
IVika-QA <apple@MacBook-Pro-Apple.local>
2025-01-13 23:21:55 +0300

    Refactoring files

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -3,7 +3,7 @@ from pages.BasePage import BasePageHelper
 from pages.LoginPage import LoginPageHelper
 import allure
 
-from tests.testRecovery import LOGIN_TEXT
+from tests.test_Recovery import LOGIN_TEXT
 
 BASE_URL = "https://ok.ru/"
 LOGIN_TEXT
